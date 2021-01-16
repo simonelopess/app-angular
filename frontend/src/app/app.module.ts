5504ce3a2e434aa80eb81f117f6fcd3268733474
SISTEMAFIRJAN\SIMLSILVA <simlsilva@firjan.com.br>
2021-01-16 19:24:50 -0300

    Formulário criar Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule} from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input'
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -40,7 +43,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
