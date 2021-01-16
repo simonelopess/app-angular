39d4a43415ea3ba2c76fccbcf819eca9a08e593f
SISTEMAFIRJAN\SIMLSILVA <simlsilva@firjan.com.br>
2021-01-16 19:41:26 -0300

    Obter produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input'
 
 import { HttpClientModule } from '@angular/common/http';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
